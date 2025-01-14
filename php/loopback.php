b61d4cf882d2629a2dea50d1157cfd9a2e4623c8
Rob Thomas <xrobau@gmail.com>
2025-01-14 15:05:21 +1000

    Make the loopback handler slightly useful as a shim

--- php/loopback.php
+++ php/loopback.php
@@ -4,6 +4,36 @@
 // It only binds to localhost:4680 and should be used only for
 // things that must run as root.
 
-$stderr = fopen("/dev/stderr", "w+");
-fwrite($stderr, "Um hi there\n");
-exit(9);
+require __DIR__ . "/vendor/autoload.php";
+
+$tmparr = parse_url($_SERVER['REQUEST_URI'] ?? "/test");
+
+$c = getCallable($tmparr);
+if (!is_callable($c)) {
+    print "Not callable when asking for " . json_encode($tmparr) . "\n";
+    exit(1);
+}
+$c();
+
+function getStderr()
+{
+    static $fh;
+    if (!$fh) {
+        $fh = fopen("/dev/stderr", "w+");
+    }
+    return $fh;
+}
+
+function getCallable(array $tmparr)
+{
+    $path = $tmparr['path'] ?? '/error';
+    switch ($path) {
+        case '/error':
+            return function () {
+                print "Invalid path";
+            };
+    }
+    return function () {
+        print "Unknown function\n";
+    };
+}
